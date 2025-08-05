77db0ea14561857bb4dd9fd8f79e291e9a5437ef
Abdullah Azzam <mabdazzam@outlook.com>
2025-08-05 09:57:47 -0600

    organize the script to create exchange table

--- create_exchange_table.sh
+++ create_exchange_table.sh
@@ -1,29 +1,18 @@
-#wd ~/hdd/nmhydro/coupling/VIC-MF6
-#mkdir -p grassdata
-#grass -c EPSG:26913 grassdata/vic_mf6_utm13n --text
-#
-## import coupling grid
-#v.import input=spatial_data/coupling_grid.gpkg layer=vic_cells output=vic_uzf_cells
-#v.import input=spatial_data/coupling_grid.gpkg layer=mf6_cells output=mf6_uzf_cells
-#
-#v.overlay ainput=mf6_uzf_cells binput=vic_uzf_cells output=mf6_vic_join oper=and --overwrite
-#v.db.addcolumn mf6_vic_join columns="vic_id INTEGER, mf6_id VARCHAR(6), cpl_id VARCHAR(16), area_m2 DOUBLE PRECISION"
-#v.to.db map=mf6_vic_join option=area column=area_m2 units=meters --overwrite
-#db.execute sql="UPDATE mf6_vic_join SET vic_id = b_gridcel"
-#db.execute sql="UPDATE mf6_vic_join SET mf6_id = substr('000' || CAST(a_row AS TEXT), -3, 3) || substr('000' || CAST(a_col AS TEXT), -3, 3)"
-#db.execute sql="UPDATE mf6_vic_join SET cpl_id = mf6_id || '_' || vic_id"
-
 #!/bin/sh
+
 set -e
 
 #cd ~/hdd/nmhydro/coupling/VIC-MF6
+## following two lines are required to run only once
+## to create the workspace and import grids from both models.
+
 #mkdir -p grassdata
 #grass -c EPSG:26913 grassdata/vic_mf6_utm13n --text
 
 #v.import input=spatial_data/coupling_grid.gpkg layer=vic_cells output=vic_uzf_cells --overwrite
-
 #v.import input=spatial_data/coupling_grid.gpkg layer=mf6_cells output=mf6_uzf_cells --overwrite
 
+# process starts here; useful to re run from here after changes
 echo "step 1: intersect vic and mf6 cells"
 v.overlay ainput=mf6_uzf_cells binput=vic_uzf_cells output=mf6_vic_join oper=and --overwrite
 
@@ -65,4 +54,18 @@ v.db.select mf6_vic_join columns=vic_id,mf6_id,area_m2,mf6_area_m2,vic_area_m2,m
 
 echo "step 13: export to CSV"
 v.out.ogr input=mf6_vic_join output=./mf6_vic_join_new.csv format=CSV --overwrite
+
+
+## initial trial to establish the workflow. OBSOLETED
+
+#mkdir -p grassdata
+#grass -c EPSG:26913 grassdata/vic_mf6_utm13n --text
+#v.import input=spatial_data/coupling_grid.gpkg layer=vic_cells output=vic_uzf_cells
+#v.import input=spatial_data/coupling_grid.gpkg layer=mf6_cells output=mf6_uzf_cells
+#v.overlay ainput=mf6_uzf_cells binput=vic_uzf_cells output=mf6_vic_join oper=and --overwrite
+#v.db.addcolumn mf6_vic_join columns="vic_id INTEGER, mf6_id VARCHAR(6), cpl_id VARCHAR(16), area_m2 DOUBLE PRECISION"
+#v.to.db map=mf6_vic_join option=area column=area_m2 units=meters --overwrite
+#db.execute sql="UPDATE mf6_vic_join SET vic_id = b_gridcel"
+#db.execute sql="UPDATE mf6_vic_join SET mf6_id = substr('000' || CAST(a_row AS TEXT), -3, 3) || substr('000' || CAST(a_col AS TEXT), -3, 3)"
+#db.execute sql="UPDATE mf6_vic_join SET cpl_id = mf6_id || '_' || vic_id"
 
